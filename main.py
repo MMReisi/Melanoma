5c3d3b53b71cdfb6e564315b3a8b3476653a2850
mohammd mahdi reisi <mohammad.mahdi.reisi2002@gmail.com>
2019-03-15 16:15:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class Run:
 
 if __name__ == '__main__':
     img = work_files.OS()
-    img = matplotlib.image.imread([i for i in img.run()][2])
+    img = matplotlib.image.imread([i for i in img.run()][1])
     obj = Run(img)
     obj.run()
     obj.show_images()
